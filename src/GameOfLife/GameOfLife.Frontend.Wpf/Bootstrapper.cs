4161ae9eddc43969342a78e0370ff8723c296f5f
Portikus <ahuisf@gmail.com>
2017-10-06 23:02:08 +0200

    Added the MVVM fram,ework Priosm and the DependencyContzainer Autofac

---
+++ src/GameOfLife/GameOfLife.Frontend.Wpf/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Autofac;
+using GameOfLife.Frontend.Wpf.ViewModels;
+using GameOfLife.Frontend.Wpf.Views;
+using Prism.Autofac;
+
+namespace GameOfLife.Frontend.Wpf
+{
+    public class Bootstrapper : AutofacBootstrapper
+    {
+        protected override void ConfigureContainerBuilder(ContainerBuilder builder)
+        {
+            base.ConfigureContainerBuilder(builder);
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            var shell = new ShellView ();
+            shell.Show();
+            return shell;
+        }
+    }
+}
