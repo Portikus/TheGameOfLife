dc22318aeea00cbb743e2ac87e69a623e64e024d
Portikus <ahuisf@gmail.com>
2017-10-07 21:31:59 +0200

    Added visibility for HotSpots

---
+++ src/GameOfLife/GameOfLife.Frontend.Wpf/TestConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GameOfLife.Frontend.Wpf
+{
+    public class TestConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                if (boolValue)
+                {
+                    return "X";
+                }
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
