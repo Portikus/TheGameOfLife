a4eb09a7227aeaaccfd02712a3ff5248b3d52e8b
MrPekel <jonashashagen@gmail.com>
2017-10-08 11:49:11 +0200

    Merge of changes.

--- src/GameOfLife/GameOfLife.Frontend.Wpf/Model/PlayerProvider.cs
+++ src/GameOfLife/GameOfLife.Frontend.Wpf/Model/PlayerProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Data;
 using GameOfLife.Api.Model;
 using Prism.Mvvm;
